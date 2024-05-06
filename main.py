907ccdf110a7e292d155143edc999ffbce6a416f
TheGenericUser <118926926+TheGenericUser@users.noreply.github.com>
2024-05-06 22:14:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,3 +29,21 @@ X_train_iris, X_test_iris, y_train_iris, y_test_iris = train_test_split(
 
 X_train_wine, X_test_wine, y_train_wine, y_test_wine = train_test_split(
     wine.data, wine.target, test_size=0.25, random_state=42)
+
+
+
+#Code for cross validation
+
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.metrics import make_scorer, accuracy_score
+
+iris = datasets.load_iris()
+
+clf = DecisionTreeClassifier(random_state=42)
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+iris_accuracies = cross_val_score(clf, iris.data, iris.target, cv=kf, scoring='accuracy')
+
+print(iris_accuracies)
